a977dc06d3ff35397e707f132e729475a6abe1e9
Jovanny "War" Berté <43629991+JovannyLB@users.noreply.github.com>
2025-04-23 15:08:44 -0300

    Added database requests

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { readFileSync } from "fs";
 import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "readbooks",
-  password: "admin",
-  port: 5432,
-});
+const db = new pg.Client(JSON.parse(readFileSync("./client.json")));
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  res.render("index.ejs");
+	const rawBooknotes = await db.query("select * from BookNotes");
+	const booknotes = rawBooknotes.rows;
+
+	res.render("index.ejs", { booknotes: booknotes });
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+	console.log(`Server running on port ${port}`);
 });
